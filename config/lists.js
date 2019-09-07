50a8e5aaca3b44c8cd64335397be787b19466f38
Anders Windelhed <anders.windelhed@bonnierbroadcasting.com>
2019-09-07 22:26:16 +0200

    Extracting list config

---
+++ config/lists.js
@@ -0,0 +1,29 @@
+const lists = [
+  {
+    name: 'Barngrupp 1',
+    includeCoaches: true,
+    columns: ['Reg.', 'Bet.', 'Typ', 'Moon', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
+  },
+  {
+    name: 'Barngrupp 2',
+    includeCoaches: true,
+    columns: ['Reg.', 'Bet.', 'Typ', 'Moon', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
+  },
+  {
+    name: 'Nybörjare',
+    includeCoaches: true,
+    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
+  },
+  {
+    name: 'Ungdomar',
+    includeCoaches: true,
+    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
+  },
+  {
+    name: 'Vuxna',
+    includeCoaches: false,
+    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
+  },
+]
+
+export default lists
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,8 @@
 import React from 'react'
 import fetch from 'node-fetch'
+import listConfig from '../config/lists'
 
 import './index.scss'
-
-const listConfig = [
-  {
-    name: 'Barngrupp 1',
-    includeCoaches: true,
-    columns: ['Reg.', 'Bet.', 'Typ', 'Moon', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
-  },
-  {
-    name: 'Barngrupp 2',
-    includeCoaches: true,
-    columns: ['Reg.', 'Bet.', 'Typ', 'Moon', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
-  },
-  {
-    name: 'Nybörjare',
-    includeCoaches: true,
-    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
-  },
-  {
-    name: 'Ungdomar',
-    includeCoaches: true,
-    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
-  },
-  {
-    name: 'Vuxna',
-    includeCoaches: false,
-    columns: ['Reg.', 'Bet.', 'Förnamn', 'Efternamn', 'Mobilnummer', ''],
-  },
-]
 
 class Page extends React.Component {
   constructor(props) {
